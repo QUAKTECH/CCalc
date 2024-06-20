d3f751e589cfc3daf1ae166a9db7e56712d73b97
Aeternus <longnecklion126@gmail.com>
2024-06-20 15:20:09 -0700

    Update ccalc.c

--- ccalc.c
+++ ccalc.c
@@ -28,7 +28,7 @@ Dependencies:
 
 The MIT License (MIT)
 
-Copyright (c) 2024 Apache Software Production. (ASP)
+Copyright (c) 2024 QUAKTECH
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
