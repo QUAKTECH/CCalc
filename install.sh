2fcaba6728d96b40048450a3d97d765a454ca80b
Aeternus <longnecklion126@gmail.com>
2024-06-21 15:52:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,4 +24,24 @@ do
   esac
 done
 
+
+printf "\nWould you like to remove ccalc.c?\n"
+read -r ask_remove_ccalc_c
+
+while true
+do
+  case $ask_remove_ccalc_c in
+      [Yy]* )
+          rm ccalc.c
+          break ;;
+      [Nn]* )
+        echo "ccalc.c not removed."
+        break ;;
+      * )
+        echo "Invalid input."
+  esac
+done
+
+
+
 make
